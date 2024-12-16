d97258890547c9d25c5a970cb2c3913189791bdd
Phyllis Tenkorang <phyllist8a@gmail.com>
2024-12-16 12:03:27 +0000

    Fix Task 2

---
+++ R/read_data.R
@@ -0,0 +1,8 @@
+## Task 2
+
+read_data <- function(url, destfile) {
+  download.file (url = url, destfile = destfile)
+  
+  read_xlsx(file = destfile)
+}
+read_data(url = "https://github.com/OxfordIHTM/teaching_datasets/raw/refs/heads/main/cyclones.xlsx", destfile = "R/read_data")
