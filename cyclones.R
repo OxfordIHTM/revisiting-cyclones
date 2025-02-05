b758be27da232238bc6a00e9da7e06bc5a716e96
nyashamanyeruke <nyashamanyeruke@gmail.com>
2025-02-05 11:53:17 +0000

    resolved initial hiccup.

--- cyclones.R
+++ cyclones.R
@@ -3,18 +3,39 @@
 ## Load bespoke functions in R folder and load libraries ----
 install.packages("openxlsx2")
 library(openxlsx2)
+library(readxl)
 
 ## Download and Read cyclones dataset ----
 get_dataset <- function(a, b) {
   download.file(url = a, destfile = b)
-  cyclones.data <- read_xlsx(file = b )
-}
+  }
 
 get_dataset(
   a="https://github.com/OxfordIHTM/teaching_datasets/raw/refs/heads/main/cyclones.xlsx",
             b="data/cyclones.xlsx")
 
+cyclones.data <- read_xlsx(file = "data/cyclones.xlsx" )
 
 #1. Description of the data such as number of records, any missing values, and description of relevant variables.
+
+## Data frame description
+
+dim(cyclones.data)
+
+str(cyclones.data)
+
+head(cyclones.data)
+
+tail(cyclones.data)
+
+summary(cyclones.data)
+
+summary(cyclones.data$speed)
+
+table(cyclones.data$category_code)
+
+## Quantifying missing variables
+
+## Labelling variables
 
 
