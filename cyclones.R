9010341efe748eb78acc465cb35983b026586e10
nyashamanyeruke <nyashamanyeruke@gmail.com>
2025-02-07 03:04:49 +0000

    still working on initial issue.

--- cyclones.R
+++ cyclones.R
@@ -36,6 +36,103 @@ table(cyclones.data$category_code)
 
 ## Quantifying missing variables
 
-## Labelling variables
+count_na <- function(df) {
+  
+### Count the number of NA values in each column
+  na_counts <- sapply(df, function(x) sum(is.na(x)))
+  
+### Convert the result to a data frame for better readability
+  na_counts_df <- data.frame(Column = names(na_counts), NA_Count = na_counts)
+  
+  return(na_counts_df)
+}
+
+na_summary <- count_na(cyclones.data)
+
+na_summary
+
+## Calculating average duration of a cyclone
+
+### Duration of cyclones in days
+
+cyclones.data$duration <- as.numeric(
+  difftime(cyclones.data$end,cyclones.data$start, units = "days")
+  )
+
+## Calculate the average duration and categorising data set
+average_duration <- mean(cyclones.data$duration, na.rm = TRUE)
+
+cyclones.data$duration[cyclones.data$duration <= average_duration ] <- 0
+
+cyclones.data$duration[cyclones.data$duration > average_duration] <- 1
+
+cyclones.data$duration <- factor(
+  cyclones.data$duration,
+  labels = c("below average", "above average")
+)
+
+summary(cyclones.data$duration)
+
+## Summarising continuous data through grouping
+
+summary(cyclones.data$pressure)
+
+cyclones.data$pressure_grouped <- NA
+
+cyclones.data$pressure_grouped [cyclones.data$pressure < 965]<-0
+
+cyclones.data$pressure_grouped [
+  cyclones.data$pressure >= 965 & cyclones.data$pressure < 973.9]<-1
+
+cyclones.data$pressure_grouped [
+  cyclones.data$pressure >= 973.9 & cyclones.data$pressure < 995]<-2
+
+cyclones.data$pressure_grouped [cyclones.data$pressure >= 995]<-3
+
+### Adding labels to pressure grouped data
+
+cyclones.data$pressure_grouped <- factor (
+  cyclones.data$pressure_grouped,
+  labels=c(
+    "<965", "965-973.9", "973.9-995", ">995"
+  )
+)
+
+
+#### Packages installed during construction of bar charts and histograms
+
+##### install.packages("tidyverse"), install.packages("RColorBrewer")
+##### library("tidyverse"), library("RColorBrewer")
+
+
+### Creating a bar chart for the categorized data
+
+pressure.grouped.graph <- table (cyclones.data$pressure_grouped)
+
+barplot (
+  pressure.grouped.graph,
+  horiz = FALSE,
+  xlab = "Pressure",
+  ylab = "Number of Cyclones",
+  main = "Pressure of Cyclones",
+  col=heat.colors(4)
+)
+
+### Creating a histogram for continuous speed data
+
+hist(
+  cyclones.data$speed,
+  xlim = c(25,185),
+  xlab = "speed",
+  ylab = "Number of Cyclones",
+  main = "Cyclone speeds",
+  col=heat.colors(12), density=100
+)
+
+## Categorising Cyclone speed data using a standard reference 
+### India Meteorological Department (IMD) Scale Vs General classification used
+
+
+
 
 
