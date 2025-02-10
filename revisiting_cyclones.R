49732cf417dc917a73480e7595241e914e3de891
Alaa N M Abushahla <aabushahla37@gmail.com>
2025-02-10 11:58:30 +0000

    generate data

---
+++ revisiting_cyclones.R
@@ -0,0 +1,9 @@
+#installed.packages("openxlsx2")
+
+
+download.file(url = https://github.com/OxfordIHTM/teaching_datasets/raw/refs/heads/main/cyclones.xlsx, destfile = "data")
+
+read_xlsx()
+
+download_and_read_cyclones = function(url, destfile = "data") {download.file(url = url, destfile = destifle)}
+                                      
