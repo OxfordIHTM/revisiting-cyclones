5e6475a50b98ddf73a4a7932da354dee6724dac8
Onesmus Odongo Onyango <onesmusodongo@gmail.com>
2025-02-10 11:58:27 +0000

    Still having the errors.

--- R/read_data.R
+++ R/read_data.R
@@ -6,3 +6,16 @@ read_data <- function(url, destfile) {
   read_xlsx(file = destfile)
 }
 
+### Feberuary 10 
+# Revisiting cyclones exercise ----
+
+## Load bespoke functions in R folder and load libraries ----
+for (i in list.files("R", full.names = TRUE)) source(i)
+library(openxlsx2)
+
+
+## Read cyclones dataset ----
+cyclones <- read_data(
+  url = "https://github.com/OxfordIHTM/teaching_datasets/raw/refs/heads/main/cyclones.xlsx", 
+  destfile = "data/cyclones.xlsx"
+)
